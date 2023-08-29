d3ef8b0718f0a1bf30071a75ee268d4237be838a
carlghayes <carlghayes@protonmail.com>
2023-08-29 14:08:35 -0500

    ad attributes for all

---
+++ attrib-update.ps1
@@ -0,0 +1,31 @@
+
+
+#Install PSScriptAnalyzer
+#This is a static code checker for PS modules & Scripts.
+
+
+#Import AD Module
+Import-Module ActiveDirectory
+
+
+
+
+$users = Get-ADUser -Filter *
+
+foreach ($user in $users) {
+    # Update Manager Field
+    Set-ADUser -Identity $user -Manager "CN=John Doe,OU=Managers,DC=domain,DC=com"
+
+    # Update Department Field
+    Set-ADUser -Identity $user -Title "New Department"
+
+    # Update Job Title
+    Set-ADUser -Identity $user -Title "New Job Title"
+    
+    # Update Location
+    Set-ADUser -Identity $user -Office "New Location"
+    
+    # Update Work Phone Number
+    Set-ADUser -Identity $user -OfficePhone "New Phone Number"
+}
+
---
+++ instructions.md
@@ -0,0 +1,8 @@
+This powershell script will update all of AD attributes to all users under "specified domain" in iteration.
+
+In this example the following fields are getting updated:
+
+1. Manager
+2. Department
+3. Location
+4. Phone Number
